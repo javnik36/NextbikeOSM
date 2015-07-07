024dbbe8f611cf51add11d45bac02e5f7e472c43
javnik36 <javnik36@gmail.com>
2015-07-07 22:59:07 +0200

    inClass parsing

--- nextbike_parser.py
+++ nextbike_parser.py
@@ -1,34 +1,60 @@
-import xml.etree.ElementTree as XML
-import nextbike_class as NC
+class NextbikeParser:
+    '''Aggregates Nextbike country Classes'''
 
-plik = XML.parse("nextbike.xml")
-root = plik.getroot()
+    def __init__(self, countrys=None):
+        import xml.etree.ElementTree as XML
+        import nextbike_class as NC
 
-for country in root:
-    for city in country:
-        place_list = []
-        for place in city:
-            try:
-                uid = place.attrib["uid"]
-                lat = place.attrib["lat"]
-                lon = place.attrib["lng"]
-                name = place.attrib["name"]
-                try:
-                    num = place.attrib["number"]
-                except:
-                    num = 9999 
-                try:
-                    bike_stands = place.get["bike_racks"]
-                except:
-                    bike_stands = "!"
-                try:
-                    bike_nrs = place.attrib["bike_numbers"]
-                    n = NC.Place(uid, lat, lon, name, num, bike_stands, bike_nrs)
-                except:
-                    n = NC.Place(uid, lat, lon, name, num, bike_stands)
+        plik = XML.parse("nextbike.xml")
+        root = plik.getroot()
 
-            except Exception as e:
-                print(e)
-                print(str(uid))
+        C_list = []
 
-            place_list.append(n)
+        for country in root:
+            cities_list = []
+
+            name = country.attrib["name"]
+            coun = country.attrib["country"]
+
+            C = NC.Country(name, coun)
+            for city in country:
+                place_list = []
+
+                uid = city.attrib["uid"]
+                name = city.attrib["name"]
+
+                c = NC.City(uid, name)
+
+                for place in city:
+                    try:
+                        uid = place.attrib["uid"]
+                        lat = place.attrib["lat"]
+                        lon = place.attrib["lng"]
+                        name = place.attrib["name"]
+                        try:
+                            num = place.attrib["number"]
+                        except:
+                            num = 9999
+                        try:
+                            bike_stands = place.attrib["bike_racks"]
+                        except:
+                            bike_stands = "!"
+                        try:
+                            bike_nrs = place.attrib["bike_numbers"]
+                            n = NC.Place(uid, lat, lon, name, num, bike_stands, bike_nrs)
+                        except:
+                            n = NC.Place(uid, lat, lon, name, num, bike_stands)
+
+                    except Exception as e:
+                        print(e)
+                        print(str(uid))
+
+                    place_list.append(n)
+
+                c.places = place_list
+                cities_list.append(c)
+
+            C.cities = cities_list
+            C_list.append(C)
+        self.countrys = C_list
+        #self.countrys = []
